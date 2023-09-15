bc4eb636858d1e6bec49efdde9524de2956af0bb
ori0o0p <openwund8517@gmail.com>
2023-09-15 18:48:37 +0900

    add signup request dto

---
+++ src/main/java/com/example/kodomoproject/domain/auth/controller/dto/request/SignupRequest.java
@@ -0,0 +1,15 @@
+package com.example.kodomoproject.domain.auth.controller.dto.request;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class SignupRequest {
+    private String name;
+
+    private String email;
+
+    private String password;
+
+}
