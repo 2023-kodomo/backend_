f50fe556c768662af7a43bbba840ff52794f07c1
ori0o0p <openwund8517@gmail.com>
2023-09-15 18:49:59 +0900

    add error code enum

---
+++ src/main/java/com/example/kodomoproject/global/exception/error/ErrorCode.java
@@ -0,0 +1,20 @@
+package com.example.kodomoproject.global.exception.error;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum ErrorCode {
+    ALREADY_EXIST(400, "user already exist"),
+    PASSWORD_NOT_MATCHES(400, "incorrect password"),
+    CREATION_OR_GENERATION_ERROR_FAILED(400, "creation/generation failed"),
+    INVALID_DATA(400, "invalid data"),
+
+    COMMENT_NOT_FOUND(404, "comment not found"),
+    PRODUCT_NOT_FOUND(404,"product not found"),
+    USER_NOT_FOUND(404, "user not found");
+
+    private final int httpStatus;
+    private final String message;
+}
