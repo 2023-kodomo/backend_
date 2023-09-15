54db05d664b1a7c6854980605c9292067c602c44
ori0o0p <openwund8517@gmail.com>
2023-09-15 18:48:17 +0900

    add User model

---
+++ src/main/java/com/example/kodomoproject/domain/user/entity/User.java
@@ -0,0 +1,36 @@
+package com.example.kodomoproject.domain.user.entity;
+
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import java.util.Date;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Document
+public class User {
+    @Id
+    private String id;
+
+    @NotBlank
+    private String name;
+
+    @NotBlank
+    @Pattern(regexp = "^[A-Za-z0-9+_.-]+@(.+)$")
+    private String email;
+
+    @NotBlank
+    private String password;
+
+    @NotBlank
+    @CreatedDate
+    private Date createdDate;
+
+}
