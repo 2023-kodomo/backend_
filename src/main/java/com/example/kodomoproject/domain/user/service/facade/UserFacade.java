e471e17f3ec00bbf1c42f5b7e4113a8094714756
ori0o0p <openwund8517@gmail.com>
2023-09-15 20:03:37 +0900

    add user facade (get user)

---
+++ src/main/java/com/example/kodomoproject/domain/user/service/facade/UserFacade.java
@@ -0,0 +1,25 @@
+package com.example.kodomoproject.domain.user.service.facade;
+
+import com.example.kodomoproject.domain.user.entity.User;
+import com.example.kodomoproject.domain.user.exception.UserNotFoundException;
+import com.example.kodomoproject.domain.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class UserFacade {
+    private final UserRepository userRepository;
+
+    public User getUser() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String email = authentication.getName();
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> UserNotFoundException.EXCEPTION);
+    }
+
+}
