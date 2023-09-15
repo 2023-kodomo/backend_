7571507c3c96110e8e0136c0e9054f1fa67f5e29
ori0o0p <openwund8517@gmail.com>
2023-09-15 20:20:48 +0900

    add upload date

--- src/main/java/com/example/kodomoproject/domain/product/service/ProductCreateService.java
+++ src/main/java/com/example/kodomoproject/domain/product/service/ProductCreateService.java
@@ -6,17 +6,22 @@ import com.example.kodomoproject.domain.product.repository.ProductRepository;
 import com.example.kodomoproject.domain.product.service.facade.ProductFacade;
 import com.example.kodomoproject.domain.user.entity.User;
 import com.example.kodomoproject.domain.user.service.facade.UserFacade;
+import com.example.kodomoproject.global.facade.DateFacade;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Date;
 
 @Service
 @RequiredArgsConstructor
 public class ProductCreateService {
     private final ProductRepository productRepository;
     private final UserFacade userFacade;
+    private final DateFacade dateFacade;
 
     public void execute(ProductRequest request) {
         User user = userFacade.getUser();
+        Date now = dateFacade.getNowDate();
 
         productRepository.save(Product.builder()
                 .seller(user)
@@ -24,6 +29,7 @@ public class ProductCreateService {
                 .content(request.getContent())
                 .price(request.getPrice())
                 .image(request.getImage())
+                .uploadDate(now)
                 .build());
     }
 
