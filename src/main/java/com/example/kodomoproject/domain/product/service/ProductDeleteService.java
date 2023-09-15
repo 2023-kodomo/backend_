845200c761094f394e975dc0573b9f06f1be9173
ori0o0p <openwund8517@gmail.com>
2023-09-15 20:05:22 +0900

    add product delete service

---
+++ src/main/java/com/example/kodomoproject/domain/product/service/ProductDeleteService.java
@@ -0,0 +1,20 @@
+package com.example.kodomoproject.domain.product.service;
+
+import com.example.kodomoproject.domain.product.entity.Product;
+import com.example.kodomoproject.domain.product.repository.ProductRepository;
+import com.example.kodomoproject.domain.product.service.facade.ProductFacade;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ProductDeleteService {
+    private final ProductRepository productRepository;
+    private final ProductFacade productFacade;
+
+    public void execute(String id) {
+        Product product = productFacade.getProductById(id);
+        productRepository.deleteById(product.getId());
+    }
+
+}
