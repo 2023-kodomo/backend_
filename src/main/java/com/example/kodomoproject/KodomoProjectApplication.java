0e67c3894c00382c39b8475bd1c48da897fa82fc
ori0o0p <openwund8517@gmail.com>
2023-09-15 17:47:08 +0900

    first commit

---
+++ src/main/java/com/example/kodomoproject/KodomoProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.kodomoproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KodomoProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KodomoProjectApplication.class, args);
+    }
+
+}
