35b70f66f0ce8d845df94f05ff95bef695c3b359
ori0o0p <openwund8517@gmail.com>
2023-09-15 19:07:45 +0900

    add auth filter(jwt)

---
+++ src/main/java/com/example/kodomoproject/global/security/jwt/JwtAuthFilter.java
@@ -0,0 +1,45 @@
+package com.example.kodomoproject.global.security.jwt;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Slf4j
+@RequiredArgsConstructor
+public class JwtAuthFilter extends OncePerRequestFilter {
+    private final JwtProvider jwtProvider;
+
+    private static final String HEADER = "Authorization";
+    private static final String PREFIX = "Bearer";
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
+        String token = extractToken(request);
+        if (StringUtils.hasText(token) && jwtProvider.validateToken(token)) {
+            Authentication authentication = jwtProvider.getAuthentication(token);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        } else {
+            log.error("토큰 유효 X");
+        }
+        filterChain.doFilter(request, response);
+    }
+
+    private String extractToken(HttpServletRequest request) {
+        String token = request.getHeader(HEADER);
+        if(StringUtils.hasText(token) && token.startsWith(PREFIX))
+            return token.substring(PREFIX.length());
+        return null;
+    }
+
+}
