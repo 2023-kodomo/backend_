fe80dce973d2c2a20d7b506ed74e541b11f9af89
ori0o0p <openwund8517@gmail.com>
2023-09-15 19:32:39 +0900

    add login

--- src/main/java/com/example/kodomoproject/domain/auth/controller/AuthController.java
+++ src/main/java/com/example/kodomoproject/domain/auth/controller/AuthController.java
@@ -1,6 +1,9 @@
 package com.example.kodomoproject.domain.auth.controller;
 
+import com.example.kodomoproject.domain.auth.controller.dto.request.LoginRequest;
 import com.example.kodomoproject.domain.auth.controller.dto.request.SignupRequest;
+import com.example.kodomoproject.domain.auth.controller.dto.response.LoginResponse;
+import com.example.kodomoproject.domain.auth.service.LoginService;
 import com.example.kodomoproject.domain.auth.service.SignupService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,10 +16,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/auth")
 public class AuthController {
     private final SignupService signupService;
+    private final LoginService loginService;
 
-    @PostMapping
+    @PostMapping("/signup")
     public void signup(@RequestBody SignupRequest request) {
         signupService.execute(request);
     }
-
+    
+    @PostMapping("/login")
+    public LoginResponse login(@RequestBody LoginRequest request) {
+        return loginService.execute(request);
+    }
 }
