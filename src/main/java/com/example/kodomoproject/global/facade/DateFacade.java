6907a2934b8f6b5eff9cc3786742b8446434b3a2
ori0o0p <openwund8517@gmail.com>
2023-09-15 20:20:32 +0900

    add date facade

---
+++ src/main/java/com/example/kodomoproject/global/facade/DateFacade.java
@@ -0,0 +1,17 @@
+package com.example.kodomoproject.global.facade;
+
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
+
+@Component
+public class DateFacade {
+
+    public Date getNowDate() {
+        LocalDateTime localDateTime = LocalDateTime.now();
+        ZoneId zoneId = ZoneId.systemDefault();
+        return Date.from(localDateTime.atZone(zoneId).toInstant());
+    }
+}
