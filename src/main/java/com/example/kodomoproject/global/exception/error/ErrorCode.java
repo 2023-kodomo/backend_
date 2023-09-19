90968c8a569de0579eb3b97149c75b0fc46b4995
ori0o0p <openwund8517@gmail.com>
2023-09-19 22:41:36 +0900

    edit errorcode

--- src/main/java/com/example/kodomoproject/global/exception/error/ErrorCode.java
+++ src/main/java/com/example/kodomoproject/global/exception/error/ErrorCode.java
@@ -6,14 +6,14 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum ErrorCode {
-    ALREADY_EXIST(400, "user already exist"),
-    PASSWORD_NOT_MATCHES(400, "incorrect password"),
-    CREATION_OR_GENERATION_ERROR_FAILED(400, "creation/generation failed"),
-    INVALID_DATA(400, "invalid data"),
+    ALREADY_EXIST(400, "유저가 이미 존재합니다."),
+    PASSWORD_NOT_MATCHES(400, "비밀번호가 일치하지 않습니다."),
+    CREATION_OR_GENERATION_ERROR_FAILED(400, "생성 실패"),
+    INVALID_DATA(400, "잘못된 데이터"),
 
-    COMMENT_NOT_FOUND(404, "comment not found"),
-    PRODUCT_NOT_FOUND(404,"product not found"),
-    USER_NOT_FOUND(404, "user not found");
+    COMMENT_NOT_FOUND(404, "댓글을 찾지 못했습니다."),
+    PRODUCT_NOT_FOUND(404,"상품을 찾지 못했습니다."),
+    USER_NOT_FOUND(404, "유저를 찾지 못했습니다.");
 
     private final int httpStatus;
     private final String message;
